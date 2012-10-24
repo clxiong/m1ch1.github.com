02a9e1009f4a79516f77e6a4104ab0b879a3bba1
Michi Mutsuzaki <michi@cs.stanford.edu>
2012-10-24 19:12:27 -0400

    added zprofile

--- setup.sh
+++ setup.sh
@@ -2,12 +2,15 @@ rm -f $HOME/.bashrc
 rm -f $HOME/.bash_profile
 rm -f $HOME/.screenrc
 rm -f $HOME/.vimrc
+rm -f $HOME/.zshrc
+rm -f $HOME/.zprofile
 
 ln -s `pwd`/bashrc $HOME/.bashrc
 ln -s `pwd`/bash_profile $HOME/.bash_profile
 ln -s `pwd`/screenrc $HOME/.screenrc
 ln -s `pwd`/vimrc $HOME/.vimrc
 ln -s `pwd`/zshrc $HOME/.zshrc
+ln -s `pwd`/zprofile $HOME/.zprofile
 
 # git config
 git config --global core.editor vim
